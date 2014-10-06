906d9074022f5ae7c0e0608aa9eff1096dc43b1e
Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
2014-10-06 18:32:21 +1100

    Remove superfluous collection size check.

--- tests/test_graph_visitors.cpp
+++ tests/test_graph_visitors.cpp
@@ -26,9 +26,7 @@ BOOST_AUTO_TEST_CASE(test_vertex_depth)
 {
     vector<size_t> L;
     boost::depth_first_search(g, boost::visitor(make_vertex_depth(back_inserter(L))));
-    BOOST_CHECK_EQUAL(L.size(), this->L.size());
-    if(L.size() == this->L.size())
-        BOOST_CHECK_EQUAL_COLLECTIONS(begin(L), end(L), begin(this->L), end(this->L));
+    BOOST_CHECK_EQUAL_COLLECTIONS(begin(L), end(L), begin(this->L), end(this->L));
 }
 
 
@@ -36,9 +34,7 @@ BOOST_AUTO_TEST_CASE(test_eulerian_path)
 {
     vector<vertex_descriptor> E;
     boost::depth_first_search(g, boost::visitor(make_eulerian_path<vertex_descriptor>(back_inserter(E))));
-    BOOST_CHECK_EQUAL(E.size(), this->E.size());
-    if(E.size() == this->E.size())
-        BOOST_CHECK_EQUAL_COLLECTIONS(begin(E), end(E), begin(this->E), end(this->E));
+    BOOST_CHECK_EQUAL_COLLECTIONS(begin(E), end(E), begin(this->E), end(this->E));
 }
 
 
