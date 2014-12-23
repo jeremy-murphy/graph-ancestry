faf4d8524078be84bc9188dd098fb46b6308a03a
Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
2014-12-23 13:05:37 +0100

    Add LSCA test file to check compilation of preprocess.

--- lsca.hpp
+++ lsca.hpp
@@ -26,6 +26,10 @@
 #define LSCA_HPP
 
 #include <boost/graph/topological_sort.hpp>
+#include <boost/graph/graph_concepts.hpp>
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/adjacency_list.hpp>
+
 #include <vector>
 #include <iterator>
 #include <algorithm>
@@ -35,24 +39,30 @@ namespace LSCA
     template <typename Graph, typename OGraph>
     // requires: VertexListGraph(Graph)
     //           OGraph is mutable
-    void preprocess(Graph const &input, OGraph &output)
+    void preprocess_lsca(Graph const &G, OGraph &output)
     {
         // requires: input is a dag.
         //           output is empty.
         /*
-         * 1. In a depth-first traversal of G, compute a topological
-         * sort L of G.
-         * 2. Initialize T_G to be the empty tree. T_G will be stored
-         * using the dynamic data structure for O(1)-LCAs from
-         * Fact 1.
+         * 1. In a depth-first traversal of G, compute a topological sort L of G.
+         * 2. Initialize T_G to be the empty tree. T_G will be stored using the 
+         * dynamic data structure for O(1)-LCAs from Fact 1.
          * 3. For all v ∈ V in the order of L: compute
          *      l = lca_T_G(parents(v))
          * as the LSA of v, and add v to T G as the child of l.
          */
         typedef typename boost::graph_traits<Graph>::vertex_descriptor vertex_descriptor;
-        std::vector<vertex_descriptor> linear_ordering;
-        boost::topological_sort(input, std::back_inserter(linear_ordering));
-        std::for_each();
+
+        BOOST_CONCEPT_ASSERT((boost::VertexListGraphConcept<Graph>)); // NOTE: This might be too strict.
+        BOOST_CONCEPT_ASSERT((boost::VertexListGraphConcept<OGraph>)); // NOTE: This might be too strict.
+        
+        auto const n = boost::num_vertices(G);
+        auto const m = boost::num_edges(G);
+        std::vector<vertex_descriptor> L;
+        boost::topological_sort(G, std::back_inserter(L));
+        std::reverse(std::begin(L), std::end(L));
+        OGraph T_G;
+        
     }
 }
 
---
+++ tests/test_lsca.cpp
@@ -0,0 +1,57 @@
+#include <boost/config.hpp>
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+
+#include "lsca.hpp"
+
+#include <boost/graph/adjacency_list.hpp>
+
+#include <iostream>
+
+#ifndef NDEBUG
+#include <boost/graph/graphviz.hpp>
+#include <fstream>
+#endif
+
+using namespace std;
+
+
+struct enable_locale
+{
+    enable_locale() { cout.imbue(locale("")); cerr.imbue(locale("")); }
+};
+
+BOOST_GLOBAL_FIXTURE(enable_locale);
+
+struct Fischer_2010_fig1
+{
+    boost::adjacency_list<> g;
+    
+    Fischer_2010_fig1()
+    {
+        boost::add_edge(0, 1, g);
+        boost::add_edge(0, 2, g);
+        boost::add_edge(1, 3, g);
+        boost::add_edge(1, 4, g);
+        boost::add_edge(2, 3, g);
+        boost::add_edge(2, 4, g);
+        boost::add_edge(2, 5, g);
+        boost::add_edge(2, 6, g);
+        boost::add_edge(5, 8, g);
+        boost::add_edge(6, 7, g);
+        boost::add_edge(6, 8, g);
+        boost::add_edge(6, 9, g);
+        boost::add_edge(7, 9, g);
+    }
+};
+
+
+BOOST_FIXTURE_TEST_CASE(empty_preprocess, Fischer_2010_fig1)
+{
+#ifndef NDEBUG
+    ofstream output("Fischer_2010_fig1.dot");
+    boost::write_graphviz(output, g);
+#endif
+    boost::adjacency_list<> h;
+    LSCA::preprocess_lsca(g, h);
+}
