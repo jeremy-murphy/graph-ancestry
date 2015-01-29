b5a133361370653ac3cd35908105944a6fad8466
Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
2015-01-29 21:11:07 +1100

    Correct/reformat some test file license and metadata.

--- tests/test_DAG.cpp
+++ tests/test_DAG.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2014  Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
+    Copyright (C) 2015  Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
--- tests/test_reflect.cpp
+++ tests/test_reflect.cpp
@@ -1,23 +1,23 @@
 /*
- *    Copyright (C) 2014  Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
- * 
- *    This program is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
- * 
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- * 
- *    You should have received a copy of the GNU General Public License
- *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+    Copyright (C) 2015  Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 /**
- * @file test_DAG.hpp
- * @brief Unit tests for DAG functions.  
+ * @file test_reflect.hpp
+ * @brief Unit tests for reflection functions.  
  */
 
 #include <boost/config.hpp>
